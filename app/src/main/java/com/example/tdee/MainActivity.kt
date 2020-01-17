5b0c6e3b48d5036215f51fe45efa186d56cf1204
yellowyetti <sudoyetti@gmail.com>
2020-01-17 01:49:57 -0600

    Isolated Login code to a seperate Launch Activity. Added a menu.

--- app/src/main/java/com/example/tdee/MainActivity.kt
+++ app/src/main/java/com/example/tdee/MainActivity.kt
@@ -15,64 +15,11 @@ import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
-    private val RC_SIGN_IN = 123
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fui_auth_method_picker_layout)
-
-        createSignInIntent()
+        setContentView(R.layout.activity_main)
     }
 
-    private fun checkCurrentUser(): Boolean {
-        val user = FirebaseAuth.getInstance().getCurrentUser()
-        return user != null
-    }
-
-    private fun createSignInIntent() {
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.EmailBuilder().build()
-        )
-
-        startActivityForResult(
-            AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders(providers)
-                .build(),
-            RC_SIGN_IN
-        )
-
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        if(resultCode == RC_SIGN_IN) {
-//            val response = IdpResponse.fromResultIntent(data)
-
-            if(resultCode == Activity.RESULT_OK) {
-                //success
-                Toast.makeText(this, "Successfully logged in", Toast.LENGTH_SHORT).show()
-            }
-            else {
-                // Sign in failed
-                Log.d("MAIN_ACTIVITY", "failed to sign in")
-            }
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        signOut()
-    }
-
-    private fun signOut() {
-        AuthUI.getInstance()
-            .signOut(this)
-            .addOnCompleteListener {
-                Log.d("MAIN_ACTIVITY", "successfully signed out")
-            }
-
-    }
 }
