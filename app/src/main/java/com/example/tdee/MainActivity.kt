ce485318a1dea4de251230e9ff982be4a142ecd6
yellowyetti <sudoyetti@gmail.com>
2020-01-17 23:47:28 -0600

    Moved FirebaseAuthUI code back into MainActivity. Added onCompleteListener for signing out. Set style for FirebaseAuthUI. Set style for MainActivity. Disabled google smart lock - it loads before showing sign in page.

--- app/src/main/java/com/example/tdee/MainActivity.kt
+++ app/src/main/java/com/example/tdee/MainActivity.kt
@@ -18,27 +18,73 @@ import com.google.firebase.auth.FirebaseUser
 
 class MainActivity : AppCompatActivity() {
 
+    private val RC_SIGN_IN = 123
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        if(!checkCurrentUser()) createSignInIntent()
         setContentView(R.layout.activity_main)
     }
 
+    // Firebase Auth
+    private fun checkCurrentUser(): Boolean {
+        val user = FirebaseAuth.getInstance().getCurrentUser()
+        return user != null
+    }
+
+    private fun createSignInIntent() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build(),
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setIsSmartLockEnabled(false)
+                .setAvailableProviders(providers)
+                .setTheme(R.style.AppTheme)
+                .build(),
+            RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(resultCode == RC_SIGN_IN) {
+        // val response = IdpResponse.fromResultIntent(data)
+
+            if(resultCode == Activity.RESULT_OK) {
+                //success
+                Log.d("LOGIN_FRAGMENT", "successfully logged in")
+            }
+            else {
+                // Sign in failed
+                Log.d("LOGIN_FRAGMENT", "failed to sign in")
+            }
+        }
+    }
+
+    // Menu
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         val inflater: MenuInflater = menuInflater
         inflater.inflate(R.menu.menu, menu)
-        return true
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.menu_item_logout -> {
                 AuthUI.getInstance()
-                    .signOut(this).isSuccessful
+                    .signOut(this)
+                    .addOnCompleteListener {
+                        this.recreate()
+                    }
+                    .isSuccessful
             }
             else -> {
-                true
+                super.onOptionsItemSelected(item)
             }
         }
     }
